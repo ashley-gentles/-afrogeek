33498c53fbd0215aac7d8ada265cd4a735dd5eb7
ashley-gentles <ashley.kimg@gmail.com>
2019-07-03 12:29:41 -0500

    UI (Updated console appearance)
    separated console into data, parameters, and analysis sections

--- transitMaps/app.R
+++ transitMaps/app.R
@@ -10,7 +10,6 @@
 library(shiny)
 library(tidytransit)
 library(leaflet)
-
 # Create string for time.
 timeString <- function(hour, tString) {
   if (hour <= 12) {
@@ -33,34 +32,97 @@ selectedDOW <- function(selection) {
   return(dow)
 }
 
+# # map display functions
+# 
+# displayEverything <- function(map) {
+#   addPolylines(data=    houston_freq[["."]][["routes_sf"]],
+#                color = ~binPal(houston_freq$.$routes_frequency$mean_headways),
+#                label = ~houston_freq[["."]][["routes_sf"]][["route_id"]],
+#                group = "mean_headway") %>% 
+#     addLegend("bottomright",
+#               pal= binPal, 
+#               values = houston_freq$.$routes_frequency$mean_headways,
+#               title = "Mean Frequency",
+#               opacity= 1, labFormat = legendLabels)
+#   
+# }
+# 
+# displaySelected <- function(map) {
+#   # frequent_routes<- dplyr::filter(houston_freq[["."]][["routes_sf"]], route_id %in% results$route_id)
+#   
+#   addPolylines(data=    frequent_routes,
+#                #color = ~binPal(houston_freq$.$routes_frequency$mean_headways),
+#                label = ~houston_freq[["."]][["routes_sf"]][["route_id"]],
+#                group = "mean_headway") %>% 
+#     addLegend("bottomright",
+#              # pal= binPal, 
+#               values = houston_freq$.$routes_frequency$mean_headways,
+#               title = "Mean Frequency",
+#               opacity= 1, labFormat = legendLabels)
+#   
+# }
+# 
+# displaySelectedPlus{
+#   # frequent_routes<- dplyr::filter(houston_freq[["."]][["routes_sf"]], route_id %in% results$route_id)
+#   
+#   addPolylines(data=    frequent_routes,
+#                #color = ~binPal(houston_freq$.$routes_frequency$mean_headways),
+#                label = ~houston_freq[["."]][["routes_sf"]][["route_id"]],
+#                group = "mean_headway") %>% 
+#   addPolylines(
+#     data = dplyr::filter(houston_freq[["."]][["routes_sf"]], route_id %in% results$route_id)
+#     
+#   )
+#     addLegend("bottomright",
+#               # pal= binPal, 
+#               values = houston_freq$.$routes_frequency$mean_headways,
+#               title = "Mean Frequency",
+#               opacity= 1, labFormat = legendLabels)
+#   
+# }
+
+
+
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
+  navbarPage("transitMaps",
+             tabPanel("About")),
   # Application title
   titlePanel("transitMaps"),
+  
+    p("Welcome to transitMaps.Use this tool to see where frequent public transportation is available in your city."),
+  p("How it works:"),
+  p("transitMaps calculates the frequency with which vehicles are scheduled to pass through routes and stops.
+    The map displays average headway, or the average amount of time between stops, for each route."),
   sidebarLayout(
     sidebarPanel (
-                    helpText("Select your city"),
-                    selectInput("city",label = "select your city", choices = c("Houston", "Ft. Collins")),
-
-                    checkboxGroupInput("days",
-                                       label="Days of week",
-                                       choiceNames = list(
-                                         "Monday", "Tuesday", "Wednesday", "Thursday",
-                                         "Friday", "Saturday", "Sunday"),
-                                       choiceValues = list(1,2,3,4,5,6,7),
-                                
-                      selected = c(1, 2, 3, 4, 5)),
-                    numericInput("todStart", value = 6, "Start time", min = 0, max = 24, step = 1),
-                    numericInput("todEnd", value = 22, "End time", min = 0, max = 24, step =1),
-                    checkboxInput("highlightToggle", label = "Select route", value = FALSE),
-                    numericInput("minHeadway", label = "minimum headway", value = 0, min = 0),
-                    numericInput("maxHeadway", label = "maximum headway", value = 15, min = 0),
-                    actionButton("createMapButton", "Create Map!"),
-                    actionButton('updateMapButton', "Update Map")
-                    ),
+      
+      wellPanel(
+        h4("Data"),
+          selectInput("city",label = "Select city / agency", choices = c("Houston - METRO", "Ft. Collins")),
+        h4("Analysis Parameters"),
+          helpText("Calculate average route frequency for the selected time frame, (i.e. show average frequencies for all bus routes Mon-Fri, from 6am-10pm)"),
+          checkboxGroupInput("days",
+                             label="Days of week",
+                             choiceNames = list("Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday", "Sunday"), 
+                             choiceValues = list(1,2,3,4,5,6,7),
+                             selected = c(1, 2, 3, 4, 5)),
+          numericInput("todStart", value = 6, "Start time", min = 0, max = 24, step = 1),
+          numericInput("todEnd", value = 22, "End time", min = 0, max = 24, step =1),
+          actionButton("createMapButton", "Create Map!")
+        ),
+      
+      wellPanel(
+        h4("Filter"),
+          helpText("Display only routes that meet the selected criteria."),
+          checkboxInput("filterToggle", label = "Filters on"),
+          numericInput("minHeadway", label = "Minimum frequency", value = 0, min = 0),
+          numericInput("maxHeadway", label = "Maximum Frequency", value = 15, min = 0)
+        )
+    ),
+    
   # 
   
   # Show a plot of the generated distribution
@@ -68,22 +130,24 @@ ui <- fluidPage(
   textOutput("parameterInfo"),
     leafletOutput("summaryPlot"),
   dataTableOutput("displayedRoutes")
+
     
   ))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-
   # hardcode to select first result for Houston data
-  houston_gtfs <- readRDS("/data/houston_gtfs_6_19.rds")
+  houston_gtfs <- readRDS("houston_gtfs")
   output$parameterInfo <- renderText({ 
     paste(
       "You're viewing average route frequencies for ",
       input$city, ", from ", timeString(input$todStart), " to ", timeString(input$todEnd), ".",
       sep = ""
       ) })
+  houston_freq <- get_route_frequency(houston_gtfs, 
+                        service_ids = c(2)
+    )
   output$summaryPlot <- renderLeaflet({
     input$createMapButton
     
@@ -98,19 +162,22 @@ server <- function(input, output) {
                        domain = houston_freq$.$routes_frequency$mean_headways,
                        bins = c(8, 16, 24, 32, 60, 120, 360))
 
-    results <- dplyr::filter(houston_freq$.$routes_frequency,
-                             mean_headways <= 15)
-    freq_network <- dplyr::filter(houston_gtfs$routes, route_id %in% results$route_id)
+    filter_results <- dplyr::filter(houston_freq$.$routes_frequency,
+                             mean_headways <= input$maxHeadway)
+    View(filter_results)
+    filter_paths <- dplyr::filter(houston_freq[["."]][["routes_sf"]], route_id %in% filter_results$route_id )
+    View(filter_paths)
+
+    
     tbl <- dplyr::left_join(houston_freq$.$routes_frequency, houston_freq$routes)%>%
       dplyr::select(route_short_name, route_long_name, median_headways, mean_headways, st_dev_headways )
     
     m <- leaflet()
-
+    # houston_freq[["."]][["routes_sf"]]
     # frequent_routes<- dplyr::filter(houston_freq[["."]][["routes_sf"]], route_id %in% results$route_id)
     legendLabels<- labelFormat(suffix = " min") 
     addTiles(m) %>%
-      addPolylines(data=houston_freq[["."]][["routes_sf"]],
-                   color = ~binPal(houston_freq$.$routes_frequency$mean_headways)) %>% 
+      
       addLegend("bottomright",
                 pal= binPal, 
                 values = houston_freq$.$routes_frequency$mean_headways,
@@ -121,13 +188,10 @@ server <- function(input, output) {
     output$displayedRoutes <- renderDataTable({
       input$createMapButton
       dplyr::left_join(houston_freq$.$routes_frequency, houston_freq$routes)%>%
-        dplyr::select(route_short_name, route_long_name, median_headways, mean_headways, st_dev_headways )
-      
-      })
-    
-    
-    
-
+        dplyr::select(route_short_name, route_long_name, median_headways, mean_headways, st_dev_headways )%>%
+        dplyr::rename(`Route ID` = route_short_name, `Route Name` = route_long_name, `Median Frequency` = median_headways, `Average Frequency` = mean_headways, `Std. Dev` = st_dev_headways)
+      }, options = list(pageLength = 7))
+ 
 }
 
 # Run the application 
