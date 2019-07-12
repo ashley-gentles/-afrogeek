87a0647c447a4048d69e94dd6d7dcb178eadd769
ashley-gentles <ashley.kimg@gmail.com>
2019-07-12 15:30:13 -0500

    added ft collins dataset ; made data selection dynamic
    added a config table

--- transitMaps/app.R
+++ transitMaps/app.R
@@ -10,6 +10,10 @@
 library(shiny)
 library(tidytransit)
 library(leaflet)
+library(dplyr)
+
+gtfs_config <- readRDS("gtfs_config")
+
 # Create string for time.
 timeString <- function(hour, tString) {
   if (hour <= 12) {
@@ -43,13 +47,13 @@ ui <- fluidPage(
   p("How it works:"),
   p("transitMaps calculates the frequency with which vehicles are scheduled to pass through routes and stops.
     The map displays average headway, or the average amount of time between stops, for each route."),
- 
+
    sidebarLayout(
     sidebarPanel (
       
       wellPanel(
         h4("Data"),
-          selectInput("city",label = "Select city / agency", choices = c("Houston - METRO", "Ft. Collins")),
+          selectInput("city",label = "Select city / agency", choices = c("Houston - METRO" = "houston_metro", "Ft. Collins - Transfort" = "ftCollins_transfort")),
         h4("Analysis Parameters"),
           helpText("Calculate average route frequency for the selected time frame, (i.e. show average frequencies for all bus routes Mon-Fri, from 6am-10pm)"),
           checkboxGroupInput("days",
@@ -87,16 +91,26 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   # Loads Houston Metro GTFS data by default.
-  houston_gtfs <- readRDS("houston_gtfs")
+  # houston_gtfs <- readRDS("houston_gtfs")
+  gtfs_data <- eventReactive(input$createMapButton, ignoreNULL = TRUE,
+                            switch (input$city,
+                              "houston_metro" = readRDS("houston_gtfs"),
+                              "ftCollins_transfort" = readRDS("ftcollins_gtfs")
+                            )
+                             )
   
-  houston_freq <- eventReactive(input$createMapButton,
-      
-      get_route_frequency(houston_gtfs, 
+  houston_freq <- eventReactive(input$createMapButton,ignoreNULL = TRUE,
+    # try({
+    {
+      gtfs_data_df <- gtfs_data()
+      get_route_frequency(gtfs_data_df, 
                           start_hour =  input$todStart, 
                           end_hour = input$todEnd,
                           dow = selectedDOW(input$days),
-                          service_ids = c(2)
-      )
+                          service_ids = gtfs_config[
+                            which(gtfs_config$agency==input$city),]$service_ids
+      )}
+    # })
   )
     
 
@@ -105,8 +119,22 @@ server <- function(input, output) {
     
     # Dependency: run on click for createMapButton
     input$createMapButton
+    gtfs_data_df <- gtfs_data()
+    
+    
     route_frequencies <- houston_freq()
-   
+    View(route_frequencies)
+    cat(file=stderr(), nrow(route_frequencies$.$routes_frequency),paste("service_id = ", gtfs_data_df$calendar$service_id[1] ),  "\n")
+    # validate(need(nrow(route_frequencies$.$routes_frequency) == 0, "Frequency table is empty"), "Create a new map")
+    if(plyr::empty(route_frequencies$.$routes_frequency) ){
+      showModal(modalDialog(
+        title = "Somewhat important message",
+        "This is a somewhat important message.",
+        easyClose = TRUE,
+        footer = NULL
+      ))
+      return(addTiles(leaflet()))
+    } 
     # Create map color pallete
     binPal <- colorBin("viridis", 
                        domain = route_frequencies$.$routes_frequency$mean_headways,
@@ -114,9 +142,9 @@ server <- function(input, output) {
   
     # Filter data on current criteria, display results on the map.
     # TODO: separate this from the RUN ANALYSIS  task.  should be reactive.  
-    filter_paths <- eventReactive(input$updateFilterButton,
-      {filter_results <- dplyr::filter(route_frequencies$.$routes_frequency,
-                                      dplyr::between(route_frequencies$.$routes_frequency$mean_headways, input$minHeadway, input$maxHeadway))
+    filter_paths <- eventReactive(input$updateFilterButton,ignoreNULL = FALSE,
+      {filter_results <- filter(route_frequencies$.$routes_frequency,
+                                      between(route_frequencies$.$routes_frequency$mean_headways, input$minHeadway, input$maxHeadway))
       dplyr::filter(route_frequencies[["."]][["routes_sf"]], route_id %in% filter_results$route_id )}
       )
 
@@ -147,17 +175,14 @@ server <- function(input, output) {
   # Display data table
     output$displayedRoutes <- renderDataTable({
       input$createMapButton
+      try({
       route_frequencies <- houston_freq()
       # Generate table of route averages for display
  
-      
-      tbl <- dplyr::left_join(route_frequencies$.$routes_frequency, route_frequencies$routes)%>%
-        dplyr::select(route_short_name, route_long_name, median_headways, mean_headways, st_dev_headways )
-      
       dplyr::left_join(route_frequencies$.$routes_frequency, route_frequencies$routes)%>%
         dplyr::select(route_short_name, route_long_name, median_headways, mean_headways, st_dev_headways )%>%
         dplyr::rename(`Route ID` = route_short_name, `Route Name` = route_long_name, `Median Frequency` = median_headways, `Average Frequency` = mean_headways, `Std. Dev` = st_dev_headways)
-      })
+      })})
  
 }
 
